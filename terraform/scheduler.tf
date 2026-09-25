3a33a139b35da671da2c942e96dea1229b71fa0f
Alvis <alviswhyi@gmail.com>
2026-09-25 21:06:12 +0800

    Change trigger method

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -2,7 +2,7 @@ resource "aws_scheduler_schedule" "aqicn_hourly"{
     flexible_time_window {
         mode = "OFF"
     }
-    schedule_expression = "cron(0 * * * ? *)"
+    schedule_expression = "rate(1 hour)"
     target {
         arn = aws_lambda_function.aqicn.arn
         role_arn = aws_iam_role.scheduler.arn
@@ -13,7 +13,7 @@ resource "aws_scheduler_schedule" "meteo_hourly"{
     flexible_time_window {
         mode = "OFF"
     }
-    schedule_expression = "cron(0 * * * ? *)"
+    schedule_expression = "rate(1 hour)"
     target {
         arn = aws_lambda_function.meteo.arn
         role_arn = aws_iam_role.scheduler.arn
